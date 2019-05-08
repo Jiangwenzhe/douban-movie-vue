d8ea3ccb81f96aad80fb650e45a9e3326d479cb1
Erzhuyijian <1821344102@qq.com>
2019-05-08 21:47:58 +0800

    添加panel子组件以及动态绑定title

---
+++ src/components/SearchMovie.vue
@@ -0,0 +1,22 @@
+<template>
+  <Panel title="title"></Panel>
+</template>
+
+<script>
+import Panel from './Panel'
+export default {
+  name: 'SearchMovie',
+  components: {
+    Panel,
+  },
+  data() {
+    return {
+      title: 'Search'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
