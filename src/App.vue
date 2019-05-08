df7a4baa7a7a0100eb71b4ee8a47541a226c9a81
Erzhuyijian <1821344102@qq.com>
2019-05-08 10:44:04 +0800

    为app.vue添加初始样式

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,28 @@ export default {
     padding: 0;
     box-sizing: border-box;
   }
+
+  body{
+    overflow: scroll;
+    font-family: -apple-system,BlinkMacSystemFont,Helvetica Neue,PingFang SC,Microsoft YaHei,Source Han Sans SC,Noto Sans CJK SC,WenQuanYi Micro Hei,sans-serif;
+    font-size: 12px;
+    line-height: 1.2;
+    background-color: #F3F5F7;
+    position: relative;
+    background-size: 25px 25px;
+    background-repeat:no-repeat;
+    background-attachment:scroll;
+  }
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+
+  .clearfix::after{
+    content: '';
+    display: block;
+    clear: both;
+  }
+
 </style>
