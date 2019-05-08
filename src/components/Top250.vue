9e744864b5c2fa4cd1bf9ae904afca8a5d1226c2
Erzhuyijian <1821344102@qq.com>
2019-05-08 21:37:15 +0800

    添加panel自组件以及标题

---
+++ src/components/Top250.vue
@@ -0,0 +1,22 @@
+<template>
+  <Panel title="Top250"></Panel>
+</template>
+
+<script>
+import Panel from './Panel'
+export default {
+  name: 'Top250',
+  components: {
+    Panel
+  },
+  data() {
+    return {
+      title: 'Top250'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
