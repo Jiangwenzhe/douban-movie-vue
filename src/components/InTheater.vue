2c73ceb1b85f5bbf78d32ee0741ac7c245f608f0
Erzhuyijian <1821344102@qq.com>
2019-05-08 21:46:18 +0800

    添加panel子组件以及动态绑定title

---
+++ src/components/InTheater.vue
@@ -0,0 +1,22 @@
+<template>
+  <Panel title="title"></Panel>
+</template>
+
+<script>
+import Panel from './Panel'
+export default {
+  name: 'InTheater',
+  components: {
+    Panel,
+  },
+  data() {
+    return {
+      title: 'InTheater'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
